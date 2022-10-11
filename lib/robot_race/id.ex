556aec62ddb0c569b2bc3cb4d67793cb4a292487
Neil Lyons <nwjlyons@googlemail.com>
2022-10-11 23:04:06 +0100

    Make ID 5 chars instead of 10

--- lib/robot_race/id.ex
+++ lib/robot_race/id.ex
@@ -2,7 +2,7 @@ defmodule RobotRace.Id do
   @moduledoc false
   @type t() :: String.t()
 
-  @size 10
+  @size 5
   @alphabet "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
   @doc """
@@ -10,7 +10,8 @@ defmodule RobotRace.Id do
   """
   @spec new() :: t()
   def new() do
-    # ~15 years needed, in order to have a 1% probability of at least one collision.
+    # ~4 years needed, in order to have a 1% probability of at least one collision.
+    # when generating 1000 IDs per second
     # https://zelark.github.io/nano-id-cc/
     Nanoid.generate(@size, @alphabet)
   end
