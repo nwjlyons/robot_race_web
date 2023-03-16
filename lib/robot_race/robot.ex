4ae0843046a2d256ff934776293bb330ae9179ee
Neil Lyons <nwjlyons@googlemail.com>
2023-03-16 09:14:34 +0000

    GPT-4 prompt: Add better type specs

--- lib/robot_race/robot.ex
+++ lib/robot_race/robot.ex
@@ -12,11 +12,15 @@ defmodule RobotRace.Robot do
   @derive {Jason.Encoder, only: [:name, :score]}
   defstruct id: nil, name: "", role: :guest, score: 0
 
+  @type id() :: RobotId.t()
+  @type name() :: String.t()
+  @type score() :: non_neg_integer()
+
   @type t() :: %__MODULE__{
-          id: RobotId.t(),
-          name: String.t(),
+          id: id(),
+          name: name(),
           role: role(),
-          score: non_neg_integer()
+          score: score()
         }
 
   @type role() :: :guest | :admin
@@ -42,7 +46,7 @@ defmodule RobotRace.Robot do
       }
 
   """
-  @spec new(String.t(), role()) :: t()
+  @spec new(name(), role()) :: t()
   def new(name, role) when is_binary(name) and is_role(role) do
     %__MODULE__{id: RobotId.new(), name: name, role: role}
   end
