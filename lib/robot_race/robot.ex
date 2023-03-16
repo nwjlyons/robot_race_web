bcb04fa38d1e94c94d676ddbafd1484e70edbb18
Neil Lyons <nwjlyons@googlemail.com>
2023-03-16 08:45:39 +0000

    GPT4 docs

--- lib/robot_race/robot.ex
+++ lib/robot_race/robot.ex
@@ -1,6 +1,8 @@
 defmodule RobotRace.Robot do
   @moduledoc """
-  Robot.
+  A module representing a Robot in a Robot Race.
+
+  A Robot has an id, name, role, and score. The role can be either `:guest` or `:admin`.
   """
 
   alias RobotRace.RobotId
@@ -22,7 +24,23 @@ defmodule RobotRace.Robot do
   defguard is_role(role) when role in @roles
 
   @doc """
-  New robot.
+  Creates a new Robot instance.
+
+  ## Parameters
+
+  - name: The name of the robot as a binary string.
+  - role: The role of the robot, which can be either `:guest` or `:admin`.
+
+  ## Examples
+
+      iex> robot = RobotRace.Robot.new("Robot 1", :guest)
+      %RobotRace.Robot{
+        id: "some_id",
+        name: "Robot 1",
+        role: :guest,
+        score: 0
+      }
+
   """
   @spec new(String.t(), role()) :: t()
   def new(name, role) when is_binary(name) and is_role(role) do
